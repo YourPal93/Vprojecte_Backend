745547b1d8725a6a178b15af2be966c591842e46
Buddy <harry.nich@bk.ru>
2022-11-20 12:53:15 +0300

    initial commit

---
+++ src/main/java/com/friend/your/vprojecte/VprojecteApplication.java
@@ -0,0 +1,13 @@
+package com.friend.your.vprojecte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VprojecteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VprojecteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/friend/your/vprojecte/VprojecteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friend.your.vprojecte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VprojecteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
