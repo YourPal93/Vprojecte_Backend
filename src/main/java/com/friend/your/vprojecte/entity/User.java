53244c58169c28e91a8bec85ecad9e82e4f05f70
Buddy <harry.nich@bk.ru>
2022-11-20 13:13:59 +0300

    update User class to add email

--- src/main/java/com/friend/your/vprojecte/entity/User.java
+++ src/main/java/com/friend/your/vprojecte/entity/User.java
@@ -10,14 +10,16 @@ public class User {
     private int id;
     private String name;
     private String login;
+    private String email;
     private LocalDate birthdate;
 
     public User() {
     }
 
-    public User(String name, String login, LocalDate birthdate) {
+    public User(String name, String login, String email, LocalDate birthdate) {
         this.name = name;
         this.login = login;
+        this.email = email;
         this.birthdate = birthdate;
     }
 }
