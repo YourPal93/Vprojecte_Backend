162ccb806d6642825716a69ba248ddbc7d08840f
YourPal93 <harry.nich@bk.ru>
2023-01-24 16:41:57 +0300

    add media

--- src/main/java/com/friend/your/vprojecte/utility/SecurityKeys.java
+++
@@ -1,9 +0,0 @@
-package com.friend.your.vprojecte.utility;
-
-public class SecurityKeys {
-
-    // TODO: SecurityKeys: add file with proper security keys
-    public static String getDefault() {
-        return "secret";
-    }
-}
