6f047cb97add1523a38b8d43e64e6c4ace6e4553
YourPal93 <harry.nich@bk.ru>
2023-03-05 18:23:34 +0300

    added cloud server

--- vprojecte-cloud-server/src/main/java/com/friend/your/vprojecte/vprojectecloudserver/VprojecteCloudServerApplication.java
+++ vprojecte-cloud-server/src/main/java/com/friend/your/vprojecte/vprojectecloudserver/VprojecteCloudServerApplication.java
@@ -2,8 +2,10 @@ package com.friend.your.vprojecte.vprojectecloudserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class VprojecteCloudServerApplication {
 
 	public static void main(String[] args) {
